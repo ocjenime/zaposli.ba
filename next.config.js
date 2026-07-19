5f7fc2415597bcac364e1dd9d027ebcf010483c9
Ivan Blazek <ivanovmail92@icloud.com>
2026-07-19 19:26:48 +0200

    feat: configure GitHub Pages deployment with Actions

--- next.config.js
+++ next.config.js
@@ -1,13 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',
   images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'images.unsplash.com',
-      },
-    ],
+    unoptimized: true,
   },
+  trailingSlash: true,
 };
 
 module.exports = nextConfig;
